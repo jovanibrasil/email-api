620a79deddc2c8e93fa8a257baf11bdee85e2927
Jovani Oliveira Brasil <jovanibrasil@gmail.com>
2019-11-30 02:03:33 -0300

    Add email service interface

--- src/main/java/com/email/controllers/EmailController.java
+++ src/main/java/com/email/controllers/EmailController.java
@@ -1,54 +1,36 @@
 package com.email.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import com.email.services.EmailService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.email.models.CustomMessage;
-import com.email.services.EmailService;
 
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping("/emails")
 public class EmailController {
-	
-	@Value("${smtp.user}")
-	private String smtpUser;
-	@Value("${smtp.password}")
-	private String smtpPassword;
-	@Value("${smtp.host}")
-	private String smtpHost;
-	@Value("${pop3.user}")
-	private String pop3User;
-	@Value("${pop3.password}")
-	private String pop3Password;
-	@Value("${pop3.host}")
-	private String pop3Host;
-	
-	@Autowired
+
 	private EmailService email;
-	
+
+	public EmailController(EmailService email) {
+		this.email = email;
+	}
+
 	@PostMapping
 	public ResponseEntity<?> sendEmail(@RequestBody CustomMessage customMessage){
-	try {
-			email.sendEmail(customMessage, smtpUser, smtpPassword, smtpHost);
+		try {
+
+			email.send(customMessage);
 
 			//String htmlText = "<H1>Hello</H1><img src=\"cid:image\">";
 			//String htmlType = "text/html";
 			//email = formatEmailContent(htmlText, htmlType, "logo.png", "image");
-			//sendEmail(email);
-			
 			//getEmails();
-			
-			// reply email
-			// forward email
 			// delete email
-			
 			// templates
 			
 			return ResponseEntity.ok().body(null);
