5b5cd90fca68bc470c4e6ffd36a6d463bd9810c4
Jovani Oliveira Brasil <jovanibrasil@gmail.com>
2019-11-30 03:25:28 -0300

    Refactoring the code: Add an email formatter class

--- src/main/java/com/email/services/EmailServiceImpl.java
+++ src/main/java/com/email/services/EmailServiceImpl.java
@@ -1,37 +1,18 @@
 package com.email.services;
 
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.Date;
-import java.util.Properties;
-
-import javax.mail.Address;
-import javax.mail.BodyPart;
-import javax.mail.Folder;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Multipart;
-import javax.mail.Part;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Store;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeBodyPart;
-import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
-
+import com.email.models.CustomMessage;
+import com.email.utils.EmailFormatter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import com.email.models.CustomMessage;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Properties;
 
 @Service
 public class EmailServiceImpl implements EmailService {
@@ -45,10 +26,16 @@ public class EmailServiceImpl implements EmailService {
 	@Value("${server.host}")
 	private String host;
 
+	private EmailFormatter emailFormatter;
+
+	public EmailServiceImpl(EmailFormatter emailFormatter) {
+		this.emailFormatter = emailFormatter;
+	}
+
 	@Override
 	public void send(CustomMessage customMessage) {
 		try {
-			Multipart multipartEmail = formatEmailContent(customMessage.getText(),
+			Multipart multipartEmail = emailFormatter.formatEmailContent(customMessage.getText(),
 					customMessage.getTextType(), "file.txt", "file");
 
 			Session session = smtpConnect(user, password, host);
@@ -142,39 +129,6 @@ public class EmailServiceImpl implements EmailService {
 		return session;
 	}
 
-
-	public Multipart formatEmailContent(String content, String type, String attachmentName, String attachmentType) {
-
-		try {
-
-			// Create the message body with multiple parts
-			Multipart multipart = new MimeMultipart();
-
-			// Create the body part
-			BodyPart messageBodyPart = new MimeBodyPart(); // Create the message part
-			messageBodyPart.setContent(content, type);
-			multipart.addBodyPart(messageBodyPart);
-
-			// Create the part two that is an attachment
-			//messageBodyPart = new MimeBodyPart();
-			//String filename = attachmentName;
-			//DataSource source = new FileDataSource(filename);
-			//messageBodyPart.setDataHandler(new DataHandler(source));
-			//messageBodyPart.setFileName(filename);
-			//multipart.addBodyPart(messageBodyPart);
-
-			//if (attachmentType.equals("image"))
-			//	messageBodyPart.setHeader("Content-ID", "<image>");
-
-			return multipart;
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return null;
-
-	}
-
 	public void getEmails(final String user, final String password, String host) {
 
 		Session session = smtpConnect(user, password, host);
@@ -204,7 +158,7 @@ public class EmailServiceImpl implements EmailService {
 			// Fetching emails - open the mail box and check the header and the content of
 			// each message
 			for (Message message : messages) {
-				writePart(message);
+				emailFormatter.writePart(message);
 				String line = reader.readLine();
 				if ("YES".equals(line)) {
 					message.writeTo(System.out);
@@ -224,116 +178,6 @@ public class EmailServiceImpl implements EmailService {
 
 	}
 
-	/*
-	 * This method checks for content-type based on which, it processes and fetches
-	 * the content of the message
-	 */
-	public void writePart(Part p) throws Exception {
-		if (p instanceof Message)
-			// Call methos writeEnvelope
-			writeEnvelope((Message) p);
 
-		System.out.println("----------------------------");
-		System.out.println("CONTENT-TYPE: " + p.getContentType());
-
-		// check if the content is plain text
-		if (p.isMimeType("text/plain")) {
-			System.out.println("This is plain text");
-			System.out.println("---------------------------");
-			System.out.println((String) p.getContent());
-		}
-		// check if the content has attachment
-		else if (p.isMimeType("multipart/*")) {
-			System.out.println("This is a Multipart");
-			System.out.println("---------------------------");
-			Multipart mp = (Multipart) p.getContent();
-			int count = mp.getCount();
-			for (int i = 0; i < count; i++)
-				writePart(mp.getBodyPart(i));
-		}
-		// check if the content is a nested message
-		else if (p.isMimeType("message/rfc822")) {
-			System.out.println("This is a Nested Message");
-			System.out.println("---------------------------");
-			writePart((Part) p.getContent());
-		}
-		// check if the content is an inline image
-		else if (p.isMimeType("image/jpeg")) {
-			System.out.println("--------> image/jpeg");
-			Object o = p.getContent();
-
-			InputStream x = (InputStream) o;
-			// Construct the required byte array
-			System.out.println("x.length = " + x.available());
-
-			int i = 0;
-			byte[] bArray = new byte[x.available()];
-
-			while ((i = (int) ((InputStream) x).available()) > 0) {
-				int result = (int) (((InputStream) x).read(bArray));
-				if (result == -1) {
-					break;
-				}
-			}
-			FileOutputStream f2 = new FileOutputStream("/tmp/image.jpg");
-			f2.write(bArray);
-		} else if (p.getContentType().contains("image/")) {
-			System.out.println("content type" + p.getContentType());
-			File f = new File("image" + new Date().getTime() + ".jpg");
-			DataOutputStream output = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(f)));
-			com.sun.mail.util.BASE64DecoderStream test = (com.sun.mail.util.BASE64DecoderStream) p.getContent();
-			byte[] buffer = new byte[1024];
-			int bytesRead;
-			while ((bytesRead = test.read(buffer)) != -1) {
-				output.write(buffer, 0, bytesRead);
-			}
-		} else {
-			Object o = p.getContent();
-			if (o instanceof String) {
-				System.out.println("This is a string");
-				System.out.println("---------------------------");
-				System.out.println((String) o);
-			} else if (o instanceof InputStream) {
-				System.out.println("This is just an input stream");
-				System.out.println("---------------------------");
-				InputStream is = (InputStream) o;
-				is = (InputStream) o;
-				int c;
-				while ((c = is.read()) != -1)
-					System.out.write(c);
-			} else {
-				System.out.println("This is an unknown type");
-				System.out.println("---------------------------");
-				System.out.println(o.toString());
-			}
-		}
-
-	}
-
-	/*
-	 * This method would print FROM,TO and SUBJECT of the message
-	 */
-	public void writeEnvelope(Message m) throws Exception {
-		System.out.println("This is the message envelope");
-		System.out.println("---------------------------");
-		Address[] a;
-
-		// FROM
-		if ((a = m.getFrom()) != null) {
-			for (int j = 0; j < a.length; j++)
-				System.out.println("FROM: " + a[j].toString());
-		}
-
-		// TO
-		if ((a = m.getRecipients(Message.RecipientType.TO)) != null) {
-			for (int j = 0; j < a.length; j++)
-				System.out.println("TO: " + a[j].toString());
-		}
-
-		// SUBJECT
-		if (m.getSubject() != null)
-			System.out.println("SUBJECT: " + m.getSubject());
-
-	}
 
 }
